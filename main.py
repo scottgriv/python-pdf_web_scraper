6431d4be1330ac006882e537a58944b580555b30
Scott Griv <scottgrivner@gmail.com>
2023-01-02 18:10:10 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# Import Modules
+import os
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+# Define your URL
+url = "https://yourWebsiteURL"
+
+#If there is no such folder, the script will create one automatically
+folder_location = r'/YOUR/OUTPUT/FILE/PATH'
+if not os.path.exists(folder_location):os.mkdir(folder_location)
+
+response = requests.get(url)
+soup= BeautifulSoup(response.text, "html.parser")     
+for link in soup.select("a[href$='.pdf']"):
+    # Name the pdf files using the last portion of each link which are unique in this case
+    filename = os.path.join(folder_location,link['href'].split('/')[-1])
+    with open(filename, 'wb') as f:
+        f.write(requests.get(urljoin(url,link['href'])).content)
